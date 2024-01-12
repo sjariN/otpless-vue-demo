054d9d8d5eda99289c176527f54515a8db75fe99
sjariN <sagar.jariwala@otpless.com>
2024-01-12 16:13:55 +0530

    added floater

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,68 +2,16 @@
 import { onMounted } from 'vue'
 //@ts-ignore
 import { initOTPless } from '@/utils/initOtpless'
-
 onMounted(() => {
-  const urlParams = new URLSearchParams(window.location.search)
-  if (urlParams.get('ex')) initOTPless(callback)
+  initOTPless(callback)
 })
 
-const callback = (otplessUser: any) => {
-  const currentURL = window.location.href
-  const urlWithoutParams = currentURL.split('?')[0]
-  window.history.pushState(null, '', urlWithoutParams)
-  localStorage.setItem('token', otplessUser.token)
-  window.location.href = '/result'
-}
-
-const openModal = () => {
-  const urlParams = new URLSearchParams(window.location.search)
-  const paramsValue = urlParams.get('ex')
-
-  if (!paramsValue) {
-    const currentURL = window.location.href
-    const newParam1 = 'ex=true'
-    const updatedURL = `${currentURL}?${newParam1}`
-    window.history.pushState(null, '', updatedURL)
-  }
-  initOTPless(callback)
-  const modalContainer = document.getElementById('modalContainer')
-  modalContainer ? (modalContainer.style.display = 'flex') : ''
-
-  setTimeout(() => {
-    removeQueryParam('ex')
-  }, 1000)
-}
-
-const removeQueryParam = (param:any) => {
-  const url = new URL(window.location.href)
-  url.searchParams.delete(param)
-  window.history.pushState(null, '', url)
-}
-
-const closeModal = (e: any) => {
-  removeQueryParam('ex')
-  const modalContainer = document.getElementById('modalContainer')
-  if (e.target === modalContainer) {
-    modalContainer ? (modalContainer.style.display = 'none') : ''
-  }
-}
+const callback = (otplessUser:any) => {
+    localStorage.setItem('token',otplessUser.token);
+    (window as any).location.href = "/result";
+  };
 </script>
 
 <template>
-  <div class="main">
-  <div
-    className="modal-container"
-    id="modalContainer"
-    @click="closeModal"
-  >
-    <div id="otpless-login-page"></div>
-  </div>
-  <button
-    id="loginBtn"
-    @click="openModal"
-  >
-    Login with modal
-  </button>
-</div>
+  <div></div>
 </template>
