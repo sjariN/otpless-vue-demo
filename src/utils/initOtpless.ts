c610efeadd8837e014e885c20a3b984d737317f9
sjariN <sagar.jariwala@otpless.com>
2024-03-09 11:12:28 +0530

    updated sdk to v2

--- src/utils/initOtpless.ts
+++ src/utils/initOtpless.ts
@@ -2,7 +2,7 @@ export const initOTPless = (callback: Function) => {
   const otplessInit = Reflect.get(window, 'otplessInit')
 
   const loadScript = () => {
-    const isScriptLoaded = document.getElementById('otplessIdScript')
+    const isScriptLoaded = document.getElementById('otpless-sdk')
     if (isScriptLoaded) return
 
     const script = document.createElement('script')
